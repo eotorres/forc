d0f6b6be7212fde7814ee3654aa80d304f15178b
Emmanuel Orestes Torres <65071460+eotorres@users.noreply.github.com>
2023-05-22 10:47:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title('📈 Previsão automatizada de séries temporais')
 
 """
 Este aplicativo de dados usa a biblioteca de código aberto do Facebook, Prophet, para gerar automaticamente valores de previsão futura a partir de um conjunto de dados importado.
-Você poderá importar seus dados de um arquivo CSV, visualizar tendências e recursos, analisar o desempenho da previsão e, finalmente, baixar a previsão criada 😵
+Você poderá importar seus dados de um arquivo CSV e o separador precisa ser por ';', visualizar tendências e recursos, analisar o desempenho da previsão e, finalmente, baixar a previsão criada 😵
 """
 
 """
